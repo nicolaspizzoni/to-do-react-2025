31cdd14eb95527915a567266be60bb7a07abe287
Nicolas Pizzoni <nickpizzoni@outlook.com>
2025-09-17 09:13:33 -0300

    feat: configurado cva e svgr adicionado para componentização dos svgs

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import Text from "./components/text"
+import TrashIcon from './assets/icons/trashRegular.svg?react'
+import trashIcon from './assets/icons/trashRegular.svg'
+
 function App() {
 
   return (
     <>
-      <div className="text-pink-dark">Hello World</div>
+      <Text as="h1" variant="body-md-bold" className="text-pink-base">Hello World</Text>
+      <TrashIcon className="fill-green-base size-5.5"/>
+      <img src={trashIcon} className="fill-green-base size-5.5" alt="" />
     </>
   )
 }
